ee53d01315d53af02ed3449b86e0c5b62f9c5687
Nishan mal <64873305+Nishan-make@users.noreply.github.com>
2022-08-16 16:05:53 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class MyCode
+    {
+        static void Main(string[] arg)
+        {
+        /*string firstname = "Nishan";
+        string lastname = "Mal";
+        Console.WriteLine(firstname + lastname);*/
+        string name = "Mindteck";
+        string rname="";
+        for (int i=name.Length-1; i>=0; i--)
+        {
+            rname+=(name[i]);
+            Console.WriteLine(name[i]);
+        }
+        Console.WriteLine(rname);
+        }
+    }
