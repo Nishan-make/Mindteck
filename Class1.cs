f3a85c1023cca05179fa6fddd96012ae078bf040
Nishan mal <64873305+Nishan-make@users.noreply.github.com>
2022-08-17 13:33:22 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+public class Class1
+{
+    public Class1()
+    {
+        var al = new ArrayList();
+        al.Add(1);
+        al.Add("Nishan");
+        al.Add(null);
+        al.Add(" ");
+        al.Add(2.4f);
+        foreach (var ar2 in al)
+            Console.WriteLine(ar2);
+    }
+}
